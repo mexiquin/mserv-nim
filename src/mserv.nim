1c679fb1c177d4ffe711f0a49bee71c2afa262e4
Quinton <dropatuningmetal@gmail.com>
2020-02-22 11:38:30 -0700

    TODO - make the arg parser

--- src/mserv.nim
+++ src/mserv.nim
@@ -1,7 +1,9 @@
 # This is just an example to get you started. A typical hybrid package
 # uses this file as the main entry point of the application.
 
-include mservpkg/net
+import mservpkg/net
+import os
+import parseopt2
 
 # check to see if ./Server directory exists within the binary's scope
 proc checkServerFolder(): bool = 
@@ -14,12 +16,9 @@ proc checkServerFolder(): bool =
     else:
       raise newException(OSError, "Directory creation failed")
 
-# TEMP - Test if the download functions work
+# Entry point of the program
 when isMainModule:
   let binDir = os.getAppDir()
   let serverUrl = "https://launcher.mojang.com/v1/objects/bb2b6b1aefcd70dfd1892149ac3a215f6c636b07/server.jar"
 
-  # Check to see if server dir exists
-  echo "Server exists: ",checkServerFolder()
-  echo "ServerLoc: ", os.joinPath(binDir, "Server")
-  echo dlFile(serverUrl, os.joinPath(binDir, "Server"))
+  # TODO create argument parser to grab the commandline arguments
